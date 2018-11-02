4d2645a7a88a54ac37c5e9da858f7109cfcb2419
kduong@stuy.edu <kduong@stuy.edu>
2018-11-02 10:57:34 -0400

    working on add functions

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,8 +8,17 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(startingCapacity);
   }
 
-  public boolean add (T newVal) {
-    
+  public boolean add(T newVal) {
+    return 
+  }
+
+  public void sortList(T newVal){
+    for (int i = 0; i < size() + 1; i++) {
+      if (newVal.compareTo(this.get(i)) <= 0) {
+        super.add(i, newVal);
+        return this.get(i) == newVal;
+      }
+    }
   }
 
 }
